257922e93fe709c9b3935c6935d2cc2762dd7b48
jp <johnamata@protonmail.com>
2022-06-15 19:42:26 +0800

    git gud

--- oop/src/arrays/Basic.java
+++ oop/src/arrays/Basic.java
@@ -6,8 +6,25 @@ public class Basic {
         //datatype[] variableNameOfArray = new datatype[sizeOfArray];
         //example
         int[] simpleIntegerArray = new int[5];
-        //another way
-        int[] simpleIntegerArray2 = {1,2,3,4,5};
+        //basically says that the reference variable "simpleIntegerArray" is pointing to an array object of size 5 "new int[5]" that contains a type of integer elements, which is denoted by the "int[]" of the left hand side of "int[] simpleIntegerArray"
+        //size must be mentioned (as see on "new int[5]") or else it'll give an error
+
+        //another way, a more direct way of creating an array. this time setting both the size and values as well in one line
+        int[] simpleIntegerArray2 = {1,2,3,4,5}; //all values must be of same type
+        //the reference variable "simpleIntegerArray2" points to the array object "{1,2,3,4,5}"
+
+        /*
+        breaking it down...
+
+        int[] simpleIntegerArray
+        --> what happens here is the declaration of the array
+        --> "integerArray" is getting defined in the stack.
+        --> the reference variable integerArray is not currently pointing to anything so it gets pointed to an object
+
+        simpleIntegerArray = new int[5]
+        --> where actual memory allocation happens
+        --> object gets created in the memory (heap)
+         */
 
     }
 }
