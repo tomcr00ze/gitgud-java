d3ae4be9233751136453458253535b021ed13650
jp <johnamata@protonmail.com>
2022-06-15 19:30:08 +0800

    git gud

---
+++ oop/src/arrays/Basic.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class Basic {
+    public static void main(String[] args) {
+        //the syntax for array:
+        //datatype[] variableNameOfArray = new datatype[sizeOfArray];
+        //example
+        
+
+    }
+}
