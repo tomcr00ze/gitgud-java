cad08926b4e3601097c3ebdfc89f7602d21536f8
jp <johnamata@protonmail.com>
2022-07-20 21:14:57 +0800

    git gud

--- fundamentals-01-classes/src/io/github/tomcr00ze/Cat.java
+++ fundamentals-01-classes/src/io/github/tomcr00ze/Cat.java
@@ -2,4 +2,7 @@ package io.github.tomcr00ze; //declare name of the package/folder this class is 
 
 public class Cat {
     //class - tool of the language to describe an object
+    String name; //name is an attribute of object "Cat" and "String" is the data type of the attribute. String in java is a sequenec of characters
+    //String is  a builtin class in java
+    int age; //int - Java reserved keyword for integer, age is attribute of type age
 }
