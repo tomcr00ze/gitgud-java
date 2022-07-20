265ff793edab2eb679c4b03ab956ef0540ba4b96
jp <johnamata@protonmail.com>
2022-07-20 22:35:49 +0800

    git gud

--- fundamentals-01-classes/src/io/github/tomcr00ze/Cat.java
+++ fundamentals-01-classes/src/io/github/tomcr00ze/Cat.java
@@ -1,7 +1,11 @@
 package io.github.tomcr00ze; //declare name of the package/folder this class is located
 
+import java.sql.SQLOutput;
+
 public class Cat { //an object named cat
     //class - tool of the language to describe an object
+
+    //attributes : we have 2 here, name and age
     String name; //name is an attribute of object or our class "Cat" and "String" is the data type of the attribute. String in java is a sequenec of characters
 
     int age; //int - Java reserved keyword for integer data type, age is attribute of type age
@@ -12,4 +16,12 @@ public class Cat { //an object named cat
     primitive data types and thus all are keywords in Java:
     byte, short, int, long, float, double, char, boolean
      */
+
+    //behaviour --> represents what a specific object can do
+    //sayMeow() is the name of a method, a behaviour/task of the object cat
+    void sayMeow(){ //name of the behaviour should be an action word
+        //parenthesis and curly brcaces should come after the behaviour/method
+        //void --> keyword to say that the method wont return a value
+        System.out.println("Meow from the Cat.java file"); //prints meow to the console
+    }
 }
---
+++ fundamentals-01-classes/src/io/github/tomcr00ze/Main2.java
@@ -0,0 +1,9 @@
+package io.github.tomcr00ze;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Cat c = new Cat();
+        c.sayMeow(); //using dot operator, but this time to call the action of the behaviour of the instance c
+        //will print: "Meow from the Cat.java file"
+    }
+}
