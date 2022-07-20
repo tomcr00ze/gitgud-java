b150ee1825d55df0b1ddcccafbeb25f97341f448
jp <johnamata@protonmail.com>
2022-07-20 21:18:19 +0800

    git gud

--- fundamentals-01-classes/src/io/github/tomcr00ze/Cat.java
+++ fundamentals-01-classes/src/io/github/tomcr00ze/Cat.java
@@ -2,7 +2,14 @@ package io.github.tomcr00ze; //declare name of the package/folder this class is 
 
 public class Cat {
     //class - tool of the language to describe an object
-    String name; //name is an attribute of object "Cat" and "String" is the data type of the attribute. String in java is a sequenec of characters
-    //String is  a builtin class in java
-    int age; //int - Java reserved keyword for integer, age is attribute of type age
+    String name; //name is an attribute of object or our class "Cat" and "String" is the data type of the attribute. String in java is a sequenec of characters
+
+    int age; //int - Java reserved keyword for integer data type, age is attribute of type age
+
+    /*
+    String is  a builtin class in java, not a keyword. int is a keyword because its one of the special data types in Java called primitives
+
+    primitive data types and thus all are keywords in Java:
+    byte, short, int, long, float, double, char, boolean
+     */
 }
