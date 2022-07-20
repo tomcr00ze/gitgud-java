9689718315a7b743a83ca8f9c167b6619ddfc704
jp <johnamata@protonmail.com>
2022-07-20 21:32:49 +0800

    git gud

--- fundamentals-01-classes/src/io/github/tomcr00ze/Cat.java
+++ fundamentals-01-classes/src/io/github/tomcr00ze/Cat.java
@@ -1,6 +1,6 @@
 package io.github.tomcr00ze; //declare name of the package/folder this class is located
 
-public class Cat {
+public class Cat { //an object named cat
     //class - tool of the language to describe an object
     String name; //name is an attribute of object or our class "Cat" and "String" is the data type of the attribute. String in java is a sequenec of characters
 
---
+++ fundamentals-01-classes/src/io/github/tomcr00ze/Main.java
@@ -0,0 +1,15 @@
+package io.github.tomcr00ze;
+
+public class Main { //classes start with a capital letter
+    public static void main(String[] args) { //main method of our program, the place where execution of the program starts from
+        //what can we do inside the main method/begining of our program?
+        Cat c; //we can declare a variable/identifier to store the reference of an instance of the Cat class
+        c = new Cat(); //to create an instance of the class
+        //we can create as many instances of Cats as we want, like
+        /*
+        new Cat();
+        new Cat();
+        new Cat();
+         */
+    }
+}
